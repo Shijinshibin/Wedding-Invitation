8a42932230248bd7f1743ad895bf18e2628e51e2
shibin-sreenivasan <shibin.sreenivasan.work@gmail.com>
2026-08-21 14:21:29 +0530

    Fix wedding music path

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -8,15 +8,13 @@ import {
 import "./MusicPlayer.css";
 
 const MusicPlayer = forwardRef((props, ref) => {
-
   const audioRef = useRef(null);
 
   useImperativeHandle(ref, () => ({
-
     playMusic() {
       if (audioRef.current) {
         audioRef.current.play().catch((err) => {
-          console.log(err);
+          console.log("Music play error:", err);
         });
       }
     },
@@ -25,37 +23,29 @@ const MusicPlayer = forwardRef((props, ref) => {
       if (audioRef.current) {
         audioRef.current.pause();
       }
-    }
-
+    },
   }));
 
   useEffect(() => {
-
     if (audioRef.current) {
-
       audioRef.current.volume = 0.4;
-
     }
-
   }, []);
 
   return (
-
     <audio
       ref={audioRef}
       loop
       preload="auto"
     >
-
       <source
-        src="/music/wedding.mp3"
+        src={`${import.meta.env.BASE_URL}music/wedding.mp3`}
         type="audio/mpeg"
       />
 
+      Your browser does not support the audio element.
     </audio>
-
   );
-
 });
 
 export default MusicPlayer;

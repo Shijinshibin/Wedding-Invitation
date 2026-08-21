db1f61e1f6894ce3337b3cb53832a37c2230ba84
shibin-sreenivasan <shibin.sreenivasan.work@gmail.com>
2026-08-21 14:32:53 +0530

    Fix GitHub Pages media paths

--- src/components/Couple/Couple.jsx
+++ src/components/Couple/Couple.jsx
@@ -5,22 +5,24 @@ function Couple() {
     <section className="couple-section">
 
       <img
-        src="/gallery/photo4.jpg"
-        alt="Couple"
+        src={`${import.meta.env.BASE_URL}gallery/photo4.jpg`}
+        alt="Amulya and Jeeva"
         className="couple-image"
       />
 
       <h2>
-  Amulya  <span>♥</span> Jeeva
-</h2>
+        Amulya <span>♥</span> Jeeva
+      </h2>
 
-   <p>
-  <em>
-    "Two Hearts,<br />
-    One Beautiful Journey,<br />
-    Forever Together."
-  </em>
-</p>
+      <p>
+        <em>
+          "Two Hearts,
+          <br />
+          One Beautiful Journey,
+          <br />
+          Forever Together."
+        </em>
+      </p>
 
     </section>
   );
